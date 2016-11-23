9fe9f723c0f8aba5290088667547268c9bdbec52
Gaurav Sharma <sharmalabs@gmail.com>
2016-11-22 23:17:23 -0800

    Added components and services using cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { HerosComponent } from './heros/heros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroSearchComponent,
+    HerosComponent
   ],
   imports: [
     BrowserModule,
